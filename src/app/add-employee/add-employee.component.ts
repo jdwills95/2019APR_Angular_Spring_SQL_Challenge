2eba0fae6bff46ef17a36e042b9721b7d00c8a68
Joey_Wills <jdwills95@gmail.com>
2019-04-25 16:21:47 -0500

    Got validation working on add employee page

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -8,6 +8,28 @@ import { HttpClientService, Employee } from '../service/http-client.service';
 })
 export class AddEmployeeComponent implements OnInit {
 
+  states = ['AL', 'AK', 'AZ', 'AR', 'CA',
+            'CO', 'CT', 'DE', 'FL', 'GA',
+            'HI', 'ID', 'IL', 'IN', 'IA',
+            'KS', 'KY', 'LA', 'ME', 'MD',
+            'MA', 'MI', 'MN', 'MS', 'MO',
+            'MT', 'NE', 'NV', 'NH', 'NJ',
+            'NM', 'NY', 'NC', 'ND', 'OH',
+            'OK', 'OR', 'PA', 'RI', 'SC',
+            'SD', 'TN', 'TX', 'UT', 'VT',
+            'VA', 'WA', 'WV', 'WI', 'WY'];
+
+  employee = {fName: '',
+              lName: '',
+              street: '',
+              city: '',
+              state: this.states[0],
+              zip: '',
+              homephone: '',
+              cellphone: '',
+              email: '',
+              id: ''};
+
   user: Employee = new Employee('', '', '', '', '', '', '', '', '', '');
 
   constructor(
